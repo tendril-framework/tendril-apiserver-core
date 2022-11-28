8ab20fe751ba78ed69fce4681759e7b284c1ec54
Neel Aundhia <neel.aundhia@gmail.com>
2022-11-29 00:45:44 +0530

    Add user profile JSON dumps to echo endpoints

--- src/tendril/apiserver/routers/diagnostics.py
+++ src/tendril/apiserver/routers/diagnostics.py
@@ -1,5 +1,7 @@
 
 
+import json
+
 from fastapi import APIRouter
 from fastapi import Depends
 from pydantic import BaseModel
@@ -7,6 +9,7 @@ from pydantic import BaseModel
 from tendril.authn import authn_dependency
 from tendril.authn import AuthUserModel
 from tendril.authn import auth_spec
+from tendril.authn import get_user_profile
 
 from tendril.utils import log
 logger = log.get_logger(__name__, log.DEBUG)
@@ -28,20 +31,30 @@ async def echo(message: GenericMessage):
 @connection_diagnostics.post("/echo-login", dependencies=[Depends(authn_dependency)])
 async def echo_login(message: GenericMessage,
                      user: AuthUserModel = auth_spec()):
+    user_profile = get_user_profile(user)
     logger.debug("Got Incoming Test Message : {}".format(message))
     logger.debug("      from logged in user : {}".format(user.id))
     logger.debug("               with email : {}".format(user.email))
-    return {'message': message.message}
+    logger.debug(json.dumps(user_profile, indent=2))
+    return {
+        'user-profile': user_profile,
+        'message': message.message
+    }
 
 
 @connection_diagnostics.post("/echo-scope", dependencies=[Depends(authn_dependency)])
 async def echo_scoped(message: GenericMessage,
                       user: AuthUserModel = auth_spec(scopes=['system:administration'])):
+    user_profile = get_user_profile(user)
     logger.debug("Got Incoming Test Message : {}".format(message))
     logger.debug("      from logged in User : {}".format(user.id))
     logger.debug("               with email : {}".format(user.email))
     logger.debug("              with scopes : {}".format(user.permissions))
-    return {'message': message.message}
+    logger.debug(json.dumps(user_profile, indent=2))
+    return {
+        'user-profile': user_profile,
+        'message': message.message
+    }
 
 
 routers = [
